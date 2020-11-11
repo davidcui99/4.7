ac031ecd02a6896f20cdaaecd44e7a5ed99c8b7a
davidcui99 <73524412+davidcui99@users.noreply.github.com>
2020-11-11 10:31:29 +0800

    Update 4.7.py

--- 4.7.py
+++ 4.7.py
@@ -2,9 +2,32 @@ import pandas as pd
 import numpy as np
 from scipy.stats import stats
 import matplotlib.pyplot as plt
+import statsmodels.api as sm
+from scipy.stats import stats
+
+rp = pd.read_csv('m_ret_10stocks.txt', sep="\t", header=None, index_col=0)
+sp500 = pd.read_csv('m_sp500ret_3mtcm.txt', sep="\t", header=None)
+sp500=sp500.dropna(axis='columns')
 
 
-dataf = pd.read_csv('m_ret_10stocks.txt', sep="\t", header=None, index_col=0)
-data = pd.read_csv('m_sp500ret_3mtcm.txt', sep="\t", header=None)
-data=data.dropna(axis='columns')
+rm=sp500.iloc[:,1]
+rf=sp500.iloc[:,2]
+rm=rm.tolist()
+rm=sm.add_constant(rm)
+rf=rf.tolist()
+alpha=[]
+beta=[]
+residual=[]
+sigma0=np.var(rm)
+for i in range(10):
+    ri=rp.iloc[:,i]
+    ri=ri.tolist()
+    h=sm.OLS(ri, rm).fit()
+    alpha.append(h.params[0])
+    beta.append(h.params[1])
+    hp=h.resid
+    residual.append(h.resid)
 
+residual=pd.DataFrame(residual)
+sigma=np.cov(residual.T)
+print(sigma)
